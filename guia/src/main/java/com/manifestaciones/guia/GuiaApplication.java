1b61eda4ca3098a6d4982493c1712876bf987d9b
XanderBy <33516014+XanderBy@users.noreply.github.com>
2021-11-21 13:22:40 +0100

    creación proyecto backend

---
+++ guia/src/main/java/com/manifestaciones/guia/GuiaApplication.java
@@ -0,0 +1,13 @@
+package com.manifestaciones.guia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuiaApplication.class, args);
+	}
+
+}
---
+++ guia/src/test/java/com/manifestaciones/guia/GuiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manifestaciones.guia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

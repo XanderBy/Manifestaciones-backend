7fcb720e2ebc54fc35f131ec7cc2010f34d4e038
XanderBy <33516014+XanderBy@users.noreply.github.com>
2021-11-21 18:30:38 +0100

    Basico backend

---
+++ guia/src/main/java/com/manifestaciones/guia/models/Datos_Basicos.java
@@ -0,0 +1,98 @@
+package com.manifestaciones.guia.models;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="datos_basicos")
+public class Datos_Basicos {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(unique = true, nullable = false)
+	private long id;
+	
+	private String  nombre;
+	private String web;
+	private int telefono;
+	private String email;
+	private String convocatoria;
+	private String descripcion;
+	private String portada;
+	private LocalDateTime fecha_hora;
+	private String duracion;
+	
+	
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getWeb() {
+		return web;
+	}
+	public void setWeb(String web) {
+		this.web = web;
+	}
+	public int getTelefono() {
+		return telefono;
+	}
+	public void setTelefono(int telefono) {
+		this.telefono = telefono;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getConvocatoria() {
+		return convocatoria;
+	}
+	public void setConvocatoria(String convocatoria) {
+		this.convocatoria = convocatoria;
+	}
+	public String getDescripcion() {
+		return descripcion;
+	}
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+	public String getPortada() {
+		return portada;
+	}
+	public void setPortada(String portada) {
+		this.portada = portada;
+	}
+	public LocalDateTime getFecha_hora() {
+		return fecha_hora;
+	}
+	public void setFecha_hora(LocalDateTime fecha_hora) {
+		this.fecha_hora = fecha_hora;
+	}
+	public String getDuracion() {
+		return duracion;
+	}
+	public void setDuracion(String duracion) {
+		this.duracion = duracion;
+	}
+	
+	
+	
+	
+}
